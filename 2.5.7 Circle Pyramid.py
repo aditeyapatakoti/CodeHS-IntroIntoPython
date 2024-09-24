79d6c6fcc304b1b860404da7a7bfdc785c5ed215
Noah <91566343+ItsFireninja@users.noreply.github.com>
2024-09-24 08:42:41 -0500

    Update 2.5.7 Circle Pyramid.py
    Removed Functions
    
    Signed-off-by: Noah <91566343+ItsFireninja@users.noreply.github.com>

--- 2.5.7 Circle Pyramid.py
+++ 2.5.7 Circle Pyramid.py
@@ -1,15 +1,18 @@
 speed(0)
 penup()
 setposition(0,-30)
-def drawcircle():
+pendown()
+circle(50)
+penup()
+setposition(-50,-115)
+for i in range(2):
     pendown()
     circle(50)
     penup()
-setposition(-50,-115)
-for i in range(2):
-    drawcircle()
     forward(100)
 setposition(-100,-200)
 for i in range(3):
-    drawcircle()
+    pendown()
+    circle(50)
+    penup()
     forward(100)
